acc2cbe2955a8f7a938cb7b93772c10bc6bc7e92
yeunj <sayeunjin@gmail.com>
2021-02-03 21:18:20 +0900

    ADD DragAndDropFragment1.kt ADD DragAndDropFragment2.kt add TabLayout, ViewPager2

---
+++ app/src/main/java/com/nvisions/solutionsforaccessibility/DragAndDrop/DragAndDropFragment2.kt
@@ -0,0 +1,17 @@
+package com.nvisions.solutionsforaccessibility.DragAndDrop
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nvisions.solutionsforaccessibility.R
+class DragAndDropFragment2 : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_drag_and_drop2, container, false)
+    }
+
+}
