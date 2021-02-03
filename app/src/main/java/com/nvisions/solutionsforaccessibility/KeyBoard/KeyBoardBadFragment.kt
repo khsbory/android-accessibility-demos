6676286835a10c1c3436a26bb98e27ed161c9306
yeunj <sayeunjin@gmail.com>
2021-02-03 14:33:17 +0900

    FIX KeyBoardBadFragment.kt change delete key when long click

--- app/src/main/java/com/nvisions/solutionsforaccessibility/KeyBoard/KeyBoardBadFragment.kt
+++ app/src/main/java/com/nvisions/solutionsforaccessibility/KeyBoard/KeyBoardBadFragment.kt
@@ -12,6 +12,7 @@ import android.view.ViewGroup
 import android.view.accessibility.AccessibilityNodeInfo
 import android.widget.BaseAdapter
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
@@ -19,6 +20,11 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.nvisions.solutionsforaccessibility.R
 import kotlinx.android.synthetic.main.fragment_key_board_bad.*
+import kotlinx.android.synthetic.main.fragment_key_board_bad.confrimButton
+import kotlinx.android.synthetic.main.fragment_key_board_bad.editText
+import kotlinx.android.synthetic.main.fragment_key_board_bad.keyBoardView
+import kotlinx.android.synthetic.main.fragment_key_board_good.*
+import java.security.Key
 
 class KeyBoardBadFragment : Fragment() {
     lateinit var keyAdapter: KeyBoardBadAdapter
@@ -33,6 +39,16 @@ class KeyBoardBadFragment : Fragment() {
         keyBoardView.layoutManager = GridLayoutManager(requireContext(), 3)
         keyAdapter = KeyBoardBadAdapter()
         keyBoardView.adapter = keyAdapter
+        keyAdapter.itemTouchListener = object :KeyBoardBadAdapter.OnItemTouchListener {
+            override fun OnItemTouch() {
+                val editLength = editText.text.toString().length
+                if (editLength >= 1){
+                    var num = editText.text.toString().substring(0, editLength - 1)
+                    editText.setText(num)
+                }
+
+            }
+        }
 //        keyAdapter.itemTouchListener = object :KeyBoardBadAdapter.OnItemTouchListener{
 //            override fun OnItemTouch(input: String, position: Int) {
 //                if(input != "" && input != null){
@@ -67,6 +83,20 @@ class KeyBoardBadFragment : Fragment() {
                 }
 
             }
+
+            override fun OnItemLongClick(view: View, input: String, position: Int) {
+                val editLength = editText.text.toString().length
+                if (editLength >= 1){
+                    var num = ""
+                    editText.setText(num)
+                    if(num.isEmpty()){
+                        editText.announceForAccessibility("숫자 없음")
+                    }
+                    else{
+                        editText.announceForAccessibility(editText.text.toString())
+                    }
+                }
+            }
         }
         confrimButton.setOnClickListener {
             val builder = AlertDialog.Builder(requireContext())
@@ -78,19 +108,24 @@ class KeyBoardBadFragment : Fragment() {
     }
 }
 
-class KeyBoardBadAdapter : RecyclerView.Adapter<KeyBoardBadAdapter.ViewHolder>() {
+class KeyBoardBadAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    val VIEW_TYPE_NUM = 0
+    val VIEW_TYPE_DELETE = 1
+
     interface OnItemClickListener{
         fun OnItemClick(input: String, position: Int)
+        fun OnItemLongClick(view: View, input: String, position: Int)
     }
 
-//    interface OnItemTouchListener{
-//        fun OnItemTouch(input: String, position: Int)
-//    }
+    interface OnItemTouchListener{
+        fun OnItemTouch()
+    }
 
     var itemClickListener :OnItemClickListener? = null
-//    var itemTouchListener :OnItemTouchListener? = null
+    var itemTouchListener :OnItemTouchListener? = null
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    inner class NumViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var keyButton: Button
         init {
             keyButton = itemView.findViewById(R.id.content)
@@ -98,33 +133,77 @@ class KeyBoardBadAdapter : RecyclerView.Adapter<KeyBoardBadAdapter.ViewHolder>()
                 val input = keyButton.text.toString()
                 itemClickListener?.OnItemClick(input, adapterPosition)
             }
-//            keyButton.setOnTouchListener { v, event ->
-//                val input = keyButton.text.toString()
-//                itemTouchListener?.OnItemTouch(input, adapterPosition)
-//                true
-//            }
         }
     }
+
+    inner class DeleteViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var deleteKey: ImageView
+        init {
+            deleteKey = itemView.findViewById(R.id.deleteKey)
+            deleteKey.setOnTouchListener { v, event ->
+                if (event.actionMasked == MotionEvent.ACTION_UP) {
+                    itemTouchListener?.OnItemTouch()
+                    Log.d("mytag", "up")
+                }
+                false
+            }
+
+//            deleteKey.setOnClickListener {
+//                Log.d("mytag", "click")
+//                itemClickListener?.OnItemClick(it, "", adapterPosition)
+//            }
+            deleteKey.setOnLongClickListener {
+                itemClickListener?.OnItemLongClick(it, "", adapterPosition)
+                false
+            }
+        }
+    }
+
+//    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+//        var keyButton: Button
+//        init {
+//            keyButton = itemView.findViewById(R.id.content)
+//            keyButton.setOnClickListener {
+//                val input = keyButton.text.toString()
+//                itemClickListener?.OnItemClick(input, adapterPosition)
+//            }
+////            keyButton.setOnTouchListener { v, event ->
+////                val input = keyButton.text.toString()
+////                itemTouchListener?.OnItemTouch(input, adapterPosition)
+////                true
+////            }
+//        }
+//    }
 
     override fun getItemCount(): Int {
         return 12
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KeyBoardBadAdapter.ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.keyboard_item, parent, false)
-        return ViewHolder(v)
+    override fun getItemViewType(position: Int): Int {
+        if (position == 11)
+            return VIEW_TYPE_DELETE
+        else
+            return VIEW_TYPE_NUM
     }
 
-    override fun onBindViewHolder(holder: KeyBoardBadAdapter.ViewHolder, position: Int) {
-        if (holder is KeyBoardBadAdapter.ViewHolder) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if (viewType == VIEW_TYPE_NUM){
+            val v = LayoutInflater.from(parent.context).inflate(R.layout.keyboard_item, parent, false)
+            return NumViewHolder(v)
+        }
+        else{
+            val v = LayoutInflater.from(parent.context).inflate(R.layout.keyboard_delete_item, parent, false)
+            return DeleteViewHolder(v)
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is KeyBoardBadAdapter.NumViewHolder) {
             if(position < 9){
                 holder.keyButton.text = (position + 1).toString()
             }
             else if (position == 10) {
                 holder.keyButton.text = "0"
-            }
-            else if (position == 11) {
-                holder.keyButton.setBackgroundResource(R.drawable.back)
             }
         }
     }
